56059f5d0910718ad87f772dfe1f046c5ec83a24
gerard-moledo <gerard.moledo@gmail.com>
2024-08-23 07:47:17 -0400

    Updated Premake file for Visual Studio build

--- premake5.lua
+++ premake5.lua
@@ -14,13 +14,17 @@ project "OpenGL"
    targetdir "%{prj.location}/%{cfg.buildcfg}_%{cfg.platform}"
    targetname "program_vs2022"
    
+   prebuildcommands { "{COPYDIR} %[Assets] %[%{cfg.buildtarget.directory}/Assets]" }
+
    files { "include/*.hpp", "src/*.cpp", _WORKING_DIR .. "/external/glad/src/glad.c" }
 
    links { "glfw3", "gdi32", "user32", "kernel32" }
 
-   libdirs { _WORKING_DIR .. "/external/glfw-3.4-win64/lib-vc2022"}
+   libdirs { _WORKING_DIR .. "/external/glfw-3.4-win64/lib-vc2022" }
 
-   includedirs { "include", _WORKING_DIR .. "/external/glad/include", _WORKING_DIR .. "/external/glfw-3.4-win64/include" }
+   includedirs { "include", _WORKING_DIR .. "/external/glad/include", _WORKING_DIR .. "/external/glfw-3.4-win64/include", _WORKING_DIR .. "/external/glm-1.0.1" }
+
+   debugdir "%{cfg.buildtarget.absolutepath}"
 
    filter "configurations:Debug"
       defines { "DEBUG" }
