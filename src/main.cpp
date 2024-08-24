222be0ed576d5b315c39c19d98e178a972270d61
gerard-moledo <gerard.moledo@gmail.com>
2024-08-23 22:39:07 -0400

    Enabled debug output, utilized instanced rendering, utilized multiple shaders, moved frame rate calculation and changed to time-based rather than frame-based.

--- src/main.cpp
+++ src/main.cpp
@@ -9,6 +9,9 @@
 #include "Renderer.hpp"
 #include "Sprite.hpp"
 
+// ================================
+// Input Handling
+// ================================
 struct Input {
     bool space_pressed;
     bool up_pressed;
@@ -29,7 +32,15 @@ void keyboard_callback(GLFWwindow* window, int key, int scancode, int action, in
         user_input.down_pressed = true;
     }
 }
+// =================================
 
+// GL Debugging
+void debug_output_callback(GLenum source, GLenum type, GLuint id, GLenum severity, GLsizei length, const GLchar* message, const void* userParam) {
+      printf("GL CALLBACK: %s type = 0x%x, severity = 0x%x, message = %s\n",
+           ( type == GL_DEBUG_TYPE_ERROR ? "** GL ERROR **" : "" ),
+            type, severity, message );
+}
+// =================================
 constexpr int WINDOW_WIDTH = 800;
 constexpr int WINDOW_HEIGHT = 600;
 
@@ -38,7 +49,7 @@ int main() {
     // GLFW Initialize
     // ==============================
     glfwInit();
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
@@ -64,21 +75,26 @@ int main() {
         return -1;
     }
     
+    glEnable(GL_DEBUG_OUTPUT);
+    glDebugMessageCallback(debug_output_callback, (void*) 0);
     glViewport(0, 0, WINDOW_WIDTH, WINDOW_HEIGHT);
     glClearColor(0.75f, 0.75f, 0.75f, 1.0f);
     bool is_mode_lines = false;
     // ===============================
+
+    // Shader Creation
     Renderer::Create_Shader("pass");
+    Renderer::Create_Shader("instanced");
 
     // ===============================
     // Batch Creation
     // ===============================
-    constexpr int WIDTH = 86;
-    constexpr int HEIGHT = 64;
+    constexpr int WIDTH = 104;
+    constexpr int HEIGHT = 104;
     std::vector<Sprite> sprites;
     for (int i = 0; i < WIDTH * HEIGHT; ++i) {
         Sprite sprite;
-        sprite.position = glm::vec2((i % WIDTH) * 10.0f - (WIDTH - 80) * 5, (i / WIDTH) * 10.0f - (HEIGHT - 60) * 5);
+        sprite.position = glm::vec2((i % WIDTH) * 10.0f - 20, (i / WIDTH) * 10.0f - 20);
         sprite.color = glm::vec3(i % 3 / 3.0f, (i % 3 + 1) / 3.0f, (i % 3 + 2) / 3.0f);
         sprite.update_mesh();
         sprites.emplace_back(sprite);
@@ -91,10 +107,10 @@ int main() {
         sprite.update_buffer();
         batch_spec.stream.insert(batch_spec.stream.end(), sprite.format.buffer.begin(), sprite.format.buffer.end());
     }
-    //  ==============================
+    // ===============================
 
     // ===============================
-    // Single Sprite 
+    // Individual Sprites
     // ===============================
     Renderer::Initialize_VAO("single", Renderer::shader_map.at("pass"), Vertex_Format{ 3, 3 });
 
@@ -108,19 +124,63 @@ int main() {
         300.0f, 300.0f, 0.0f, 1.0f, 1.0f, 0.0f,
         100.0f, 300.0f, 0.0f, 0.0f, 1.0f, 1.0f,
     };
-
     Renderer::Update_VAO_Stream("single");
     // ===============================
 
+    // ===============================
+    // Instanced Sprites 
+    // =============================== 
+    Renderer::Initialize_VAO("instanced", Renderer::shader_map["instanced"], Vertex_Format{ 3, 3 });
+
+    VAO_Spec& instanced_spec = Renderer::vao_spec_map.at("instanced");
+    instanced_spec.stream = std::vector<float> {
+        -0.5f, -0.5f, 0.0f, 1.0f, 0.0f, 0.0f,
+        0.5f, -0.5f, 0.0f, 1.0f, 0.0f, 1.0f,
+        0.5f,  0.5f, 0.0f, 1.0f, 1.0f, 0.0f,
+
+        -0.5f, -0.5f, 0.0f, 1.0f, 0.0f, 0.0f,
+         0.5f,  0.5f, 0.0f, 1.0f, 1.0f, 0.0f,
+        -0.5f,  0.5f, 0.0f, 0.0f, 1.0f, 1.0f,
+    };
+    
+    constexpr int INSTANCE_WIDTH = 104;
+    constexpr int INSTANCE_HEIGHT = 104;
+    instanced_spec.instance_count = INSTANCE_WIDTH * INSTANCE_HEIGHT;
+    for (int i = 0; i < instanced_spec.instance_count; ++i) {
+        glm::mat4 world_transform = glm::mat4(1.0f);
+        glm::vec3 offset = glm::vec3(   800.0f / INSTANCE_WIDTH * (i % INSTANCE_WIDTH + 0.5f), 
+                                        600.0f / INSTANCE_HEIGHT * (i / INSTANCE_WIDTH + 0.5f),
+                                        0.0f);
+        world_transform = glm::translate(world_transform, offset);
+        float* transform_data = glm::value_ptr(world_transform);
+        instanced_spec.instanced_stream.insert( instanced_spec.instanced_stream.begin() + 16 * i,
+                                                transform_data + 0, transform_data + 16);
+    }
+
+    Renderer::Update_VAO_Stream("instanced");
+    // =============================== 
+
+    // ===============================
     // Setup screen-space transform
+    // ===============================
     glUseProgram(single_spec.shader_program);
 
     unsigned int ORTHO_TRANSFORM_LOCATION = glGetUniformLocation(Renderer::shader_map.at("pass"), "ortho_transform");
-
     glm::mat4 ortho_transform = glm::mat4(1.0f);
     ortho_transform = glm::ortho(0.0f, 800.0f, 0.0f, 600.0f, 0.0f, -100.0f) * ortho_transform;
     glUniformMatrix4fv(ORTHO_TRANSFORM_LOCATION, 1, GL_FALSE, glm::value_ptr(ortho_transform));
 
+    
+    glUseProgram(instanced_spec.shader_program);
+
+    unsigned int ORTHO_INSTANCE_TRANSFORM_LOCATION = glGetUniformLocation(Renderer::shader_map.at("instanced"), "ortho_transform");
+    ortho_transform = glm::mat4(1.0f);
+    ortho_transform = glm::ortho(0.0f, 800.0f, 0.0f, 600.0f, 0.0f, -100.0f) * ortho_transform;
+    glUniformMatrix4fv(ORTHO_INSTANCE_TRANSFORM_LOCATION, 1, GL_FALSE, glm::value_ptr(ortho_transform));
+
+    unsigned int WORLD_INSTANCE_TRANSFORM_LOCATION = glGetUniformLocation(Renderer::shader_map["instanced"], "world_transform");
+    glm::mat4 world_instance_transform;
+    
     glUseProgram(0);
     // =============================
 
@@ -146,6 +206,7 @@ int main() {
         // Timing
         float current_t = (float) glfwGetTime();
         float dt = current_t - prev_t;
+        printf("%.3f\n", (float) dt);
         prev_t = current_t;
         if (dt > frame_rate * lag && lag > 0)
             dt = frame_rate * lag;
@@ -174,19 +235,33 @@ int main() {
                 stream_it = std::copy(sprite.format.buffer.begin(), sprite.format.buffer.end(), stream_it);
                 sprite.position = position;
             }
+
+            world_instance_transform = glm::mat4(1.0f);
+            world_instance_transform = glm::rotate(world_instance_transform, 0.75f * glm::pi<float>() * fmodf(current_t, 1000.0f), glm::vec3(0.0f, 0.0f, 1.0f));
+            glm::vec2 size = glm::vec2(1.0f) * 10.0f * -glm::abs(((int) (current_t * SPEED) % SPEED) / (float) SPEED - 0.5f);
+            world_instance_transform = glm::scale(world_instance_transform, glm::vec3(size, 1.0f));
         }
 
         // ===============================
         // RENDERING
         // ===============================
         glClear(GL_COLOR_BUFFER_BIT);
-
+        
+        glUseProgram(Renderer::shader_map["pass"]);
+        
         Renderer::Update_VAO_Stream("batch");
         Renderer::Draw("batch");
         
         Renderer::Draw("single");
 
-        printf("%d\n", (int) (1 / dt));
+
+        glUseProgram(Renderer::shader_map["instanced"]);
+        
+        glUniformMatrix4fv(WORLD_INSTANCE_TRANSFORM_LOCATION, 1, GL_FALSE, glm::value_ptr(world_instance_transform));
+        Renderer::Draw("instanced");
+        
+        glUseProgram(0);
+
         glfwSwapBuffers(window);
         // ===============================
     }
