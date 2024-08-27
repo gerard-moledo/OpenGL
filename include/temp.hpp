21157ce521e6221e2347c2b2376786018560dc54
gerard-moledo <gerard.moledo@gmail.com>
2024-08-27 00:43:15 -0400

    Using temp.hpp for class blueprints.

--- include/temp.hpp
+++ include/temp.hpp
@@ -1,1 +1,21 @@
-// Temporary header for source control consistency
+// Test code
+
+
+
+// class Text {
+// public:
+//     UI_Transform ui_transform;
+//     std::vector<Font> glyphs;
+// };
+
+// class Solid {
+// public:
+//     std::vector<glm::vec3> vertices;
+//     Body body;
+//     Shape debug_shape;
+// };
+
+// class Sprite {
+// public:
+
+// };
